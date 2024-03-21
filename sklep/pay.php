ce812c797494b5864bf7e22e0882b448245a512f
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-21 11:05:54 +0100

    bd
    dodawanie wszystkiego co osoba kupi do bazy danych

--- sklep/pay.php
+++ sklep/pay.php
@@ -25,7 +25,7 @@
         </nav>
 
         <div>
-            <section>
+            <form action="pay.php" method = 'post'>
                 <h2>Wprowadź rodzaj płatności</h2>
                 <?php
                     $r = mysqli_query($connect, "SELECT * FROM `płatność` WHERE `rodzaj` = 'płatność';");
@@ -47,21 +47,21 @@
 
                     while ($row = mysqli_fetch_array($r)){
                         if($row['dostępne'] == 0){
-                            echo "<input disabled type = 'radio' name = 'pay' value = '" . $row['nazwa'] . "' id = '" . $row['nazwa'] . "'>";
+                            echo "<input disabled type = 'radio' name = 'send' value = '" . $row['nazwa'] . "' id = '" . $row['nazwa'] . "'>";
                             echo "<label for = '" . $row['nazwa'] . "' class = 'niedostepne'>" . $row['nazwa'] . "</label>";
                         }
                         else{
-                            echo "<input required type = 'radio' name = 'pay' value = '" . $row['nazwa'] . "' id = '" . $row['nazwa'] . "'>";
+                            echo "<input required type = 'radio' name = 'send' value = '" . $row['nazwa'] . "' id = '" . $row['nazwa'] . "'>";
                             echo "<label for = '" . $row['nazwa'] . "'>" . $row['nazwa'] . "</label>";
                         }
                     }
                 ?>
 
                 <h2>Wprowadź adres dostawy</h2>   <!--ma się zmieniać w zależności od rodzaju wysyłki-->
-                <input type="text" placeholder = 'email lub adres'>
+                <input type="text" placeholder = 'email lub adres' name = 'email' required>
                 <br/><br/>
 
-                <button><h2>Zapłać</h2></button>
+                <button type = 'submit' name = 'submit'><h2>Zapłać</h2></button>
                 <br/><br/>
 
                 <?php
@@ -74,7 +74,7 @@
                         echo "<button><a href='login.php' class = 'log_in'>zaloguj!</a></button>";
                     }
                 ?>
-            </section>
+            </form>
 
             <aside>
                 <h3>W sumie trzeba zapłacić:</h3>
@@ -82,6 +82,21 @@
                     echo "<p>" . $_SESSION['summary'] . "zł</p>";
                 ?>
             </aside>
+
+            <?php
+                if(isset($_POST['submit'])){
+                    for($i = 0; ; $i++){
+                        if($_SESSION['koszyk']['produkt' . $i]['nazwa'] != null){
+                            $produkty = $produkty . " nazwa: " . $_SESSION['koszyk']['produkt' . $i]['nazwa'] . ", ilość: " . $_SESSION['koszyk']['produkt' . $i]['ilosc'] . ",";
+                        }
+                        else{
+                            break;
+                        }
+                    }
+
+                    mysqli_query($connect, "INSERT INTO `historia` (`id`, `płatność`, `wysyłka`, `adres`, `produkty`) VALUES (NULL, '" . $_POST['pay'] . "', '" . $_POST['send'] . "', '" . $_POST['email'] . "', '$produkty');");
+                }
+            ?>
         </div>
     </body>
 </html>
