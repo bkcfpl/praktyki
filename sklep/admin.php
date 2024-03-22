87dfb2cba0fd08d70dd615e22311209777638a61
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-22 09:50:24 +0100

    dodano edycje

--- sklep/admin.php
+++ sklep/admin.php
@@ -29,8 +29,61 @@
             <?php
                 $r = mysqli_query($connect, "SELECT * FROM `uzytkownicy` WHERE `login` = 'admin'");
                 $row = mysqli_fetch_array($r);
+
+
+                if(isset($_POST['edit_item'])){
+                    $_SESSION['item_id'] = $_POST['edit_item'] + 1;
+                    $r = mysqli_query($connect, "SELECT * FROM `produkty` WHERE `id` = '" . $_SESSION['item_id'] . "'");
+                    $row = mysqli_fetch_array($r);
+                    
+                    echo "<h1>Edycja:</h1>";
+
+                    echo "<input type = 'text' required name = 'nazwa' value = '" . $row['nazwa'] . "'><br/>";
+                    echo "<input type = 'text' required name = 'cena' value = '" . number_format($row['cena'], 2) . "'><br/>";
+                    echo "<textarea required name = 'opis' rows = '5' cols = '33'>" . $row['opis'] . "</textarea><br/>";
+
+                    echo "<button type = 'submit' name = 'submit_edit_item'>Zmień</button>";
+                }
+                else if(isset($_POST['edit_order'])){
+                    $_SESSION['order_id'] = $_POST['edit_order'];
+                    $r = mysqli_query($connect, "SELECT * FROM `historia` WHERE `id` = '" . $_SESSION['order_id'] . "'");
+                    $row = mysqli_fetch_array($r);
+                    
+                    echo "<h1>Edycja:</h1>";
+
+                    echo "<input type = 'text' required name = 'platnosc' value = '" . $row['płatność'] . "'><br/>";
+                    echo "<input type = 'text' required name = 'wysylka' value = '" . $row['wysyłka'] . "'><br/>";
+                    echo "<input type = 'text' required name = 'adres' value = '" . $row['adres'] . "'><br/>";
+                    echo "<textarea required name = 'produkty' rows = '5' cols = '33'>" . $row['produkty'] . "</textarea><br/>";
+
+                    echo "<button type = 'submit' name = 'submit_edit_order'>Zmień</button>";
+                }
+                else if(isset($_POST['edit_user'])){
+                    $_SESSION['user_id'] = $_POST['edit_user'] + 1;
+                    $r = mysqli_query($connect, "SELECT * FROM `uzytkownicy` WHERE `id` = '" . $_SESSION['user_id'] . "'");
+                    $row = mysqli_fetch_array($r);
+                    
+                    echo "<h1>Edycja:</h1>";
+
+                    echo "<input type = 'text' required name = 'login' value = '" . $row['login'] . "'><br/>";
+                    echo "<input type = 'text' required name = 'haslo' value = '" . $row['haslo'] . "'><br/>";
+
+                    echo "<button type = 'submit' name = 'submit_edit_user'>Zmień</button>";
+                }
+
+                if(isset($_POST['submit_edit_item'])){
+                    $desc = addslashes($_POST['opis']);
+
+                    mysqli_query($connect, "UPDATE `produkty` SET `nazwa` = '" . $_POST['nazwa'] . "', `cena` = '" . $_POST['cena'] . "', `opis` = '" . $desc . "' WHERE `produkty`.`id` = " . $_SESSION['item_id'] . ";");
+                }
+                else if(isset($_POST['submit_edit_order'])){
+                    mysqli_query($connect, "UPDATE `historia` SET `płatność` = '" . $_POST['platnosc'] . "', `wysyłka` = '" . $_POST['wysylka'] . "', `adres` = '" . $_POST['adres'] . "', `produkty` = '" . $_POST['produkty'] . "' WHERE `historia`.`id` = " . $_SESSION['order_id'] . ";");
+                }
+                else if(isset($_POST['submit_edit_user'])){
+                    mysqli_query($connect, "UPDATE `uzytkownicy` SET `login` = '" . $_POST['login'] . "', `haslo` = '" . $_POST['haslo'] . "' WHERE `uzytkownicy`.`id` = " . $_SESSION['user_id'] . ";");
+                }
                 
-                if($_POST['login'] == $row['login'] && $_POST['password'] == $row['haslo']){
+                if(($_POST['login'] == $row['login'] && $_POST['password'] == $row['haslo']) || isset($_POST['edit_item']) || isset($_POST['edit_order']) || isset($_POST['edit_user']) || isset($_POST['submit_edit_item']) || isset($_POST['submit_edit_order']) || isset($_POST['submit_edit_user'])){
                     $r = mysqli_query($connect, "SELECT * FROM `produkty`");
 
                     echo "<h1>Produkty:</h1>";
@@ -42,6 +95,7 @@
                                         echo "<img src='" . $row['zdjecie_link'] . "' alt='zdjęcie_przedmiotu_nr." . $i . "'>";
                                         echo "<h3>" . $row['nazwa'] ."</h3>";
                                         echo "<p>" . number_format($row['cena'], 2) . "zł</p>";
+                                        echo "<button type = 'submit' name = 'edit_item' value = $i>Edytuj</button>";
                                     echo "</div>";
                                 echo "</td>";
                             
@@ -62,14 +116,16 @@
                             echo "<td>wysyłka</td>";
                             echo "<td>adres/email</td>";
                             echo "<td>produkty</td>";
+                            echo "<td>Przycisk edycji</td>";
                         echo "</tr>";
-                        for($i = 0; $i < $row = mysqli_fetch_array($r); $i++){     
+                        for($i = 2; $i - 2 < $row = mysqli_fetch_array($r); $i++){     
                             echo "<tr class = 'zamowienie'>";
                                 echo "<td>" . $row['id'] . "</td>";
                                 echo "<td>" . $row['płatność'] . "</td>";
                                 echo "<td>" . $row['wysyłka'] . "</td>";
                                 echo "<td>" . $row['adres'] . "</td>";
                                 echo "<td>" . $row['produkty'] . "</td>";
+                                echo "<td><button type = 'submit' name = 'edit_order' value = $i>Edytuj</button></td>";
                             echo "</tr>";
                         }
                     echo "</table>";
@@ -81,13 +137,14 @@
                             echo "<td>id</td>";
                             echo "<td>login</td>";
                             echo "<td>hasło</td>";
+                            echo "<td>Przycisk edycji</td>";
                         echo "</tr>";
                         for($i = 0; $i < $row = mysqli_fetch_array($r); $i++){     
                             echo "<tr>";
                                 echo "<td>" . $row['id'] . "</td>";
                                 echo "<td>" . $row['login'] . "</td>";
                                 echo "<td>" . $row['haslo'] . "</td>";
-                                echo "</td>";
+                                echo "<td><button type = 'submit' name = 'edit_user' value = $i>Edytuj</button></td>";
                             echo "</tr>";
                         }
                     echo "</table>";
