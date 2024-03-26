bcd7da7ad0fd7a8d405598cc02286dfad56887aa
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-26 14:17:36 +0100

    dodano repeats
    dodano stronę, która oblicza ile cyfr się powtarza, i zapisuje to do bazy danych

---
+++ lotto/repeats.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <title>Powtórzenia</title>
+    </head>
+    <body>
+        <?php
+            $connection = mysqli_connect('localhost', "root", "", "lotto");
+
+            for($i = 0; $i < 99; $i++){
+                $repeats[$i] = 0;
+                $r = mysqli_query($connection, "SELECT * FROM `results` WHERE `n1` = $i OR `n2` = $i OR `n3` = $i OR `n4` = $i OR `n5` = $i OR `n6` = $i");
+                while($row = mysqli_fetch_array($r)){
+                    // echo $row['id'] . "<br/>";
+                    $repeats[$i] ++;
+                }
+
+                echo "id: " . $i . ", powtórzenia: " . $repeats[$i] . "<br/>";
+
+                $r = mysqli_query($connection, "SELECT * FROM `repeats`");
+                $row = mysqli_fetch_array($r);
+                if(empty($row['id'])){
+                    mysqli_query($connection, "INSERT INTO `repeats`(`number`, `repeats`) VALUES ('$i','" . $repeats[$i] . "')");
+                }
+            }
+        ?>
+    </body>
+</html>
