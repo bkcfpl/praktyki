4994eec85fcb671787381e05e32649312ec52a14
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-26 14:31:04 +0100

    poprawa błędu, dodanie potwierdzenia
    poprawiono błąd, który sprawiał że nie można było ponownie użyć strony, i dodano potwierdzenie udanej operacji

--- lotto/repeats.php
+++ lotto/repeats.php
@@ -12,7 +12,6 @@
                 $repeats[$i] = 0;
                 $r = mysqli_query($connection, "SELECT * FROM `results` WHERE `n1` = $i OR `n2` = $i OR `n3` = $i OR `n4` = $i OR `n5` = $i OR `n6` = $i");
                 while($row = mysqli_fetch_array($r)){
-                    // echo $row['id'] . "<br/>";
                     $repeats[$i] ++;
                 }
 
@@ -20,10 +19,14 @@
 
                 $r = mysqli_query($connection, "SELECT * FROM `repeats`");
                 $row = mysqli_fetch_array($r);
-                if(empty($row['id'])){
+                if(!empty($row['id'])){
                     mysqli_query($connection, "INSERT INTO `repeats`(`number`, `repeats`) VALUES ('$i','" . $repeats[$i] . "')");
                 }
+                else{
+                    mysqli_query($connection, "UPDATE `repeats` SET `repeats`='" . $repeats[$i] . "' WHERE `number` = $i ");
+                }
             }
+            echo "<h2>Policzno wszystkie lizcby, i zapisano je do bazy danych!</h2>";
         ?>
     </body>
 </html>
