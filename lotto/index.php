45ab347016cd0894f836df41f5f081376b23270d
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-27 11:51:40 +0100

    optymalizacja
    zoptymalizowano zapisywanie wartości do bazy

--- lotto/index.php
+++ lotto/index.php
@@ -16,6 +16,7 @@
             $plik = file("C:\Users\Praktykant\Downloads\dl.txt");
 
             if(empty($row['id'])){
+                $arr = [];
                 for($i = 0; $i <= 3999; $i++){
                     $linijka = explode(".", $plik[$i]);
                     $rok = explode(" ", $linijka[3]);
@@ -24,7 +25,12 @@
                     $id = $linijka[0];
                     echo $data = $rok[0] . "-" . $linijka[2] . "-" . trim($linijka[1]);
 
-                    mysqli_query($connection, "INSERT INTO `results`(`id`, `result-date`, `n1`, `n2`, `n3`, `n4`, `n5`, `n6`) VALUES ('" . $id . "','" . $data . "','" . $liczba[0] . "','" . $liczba[1] . "','" . $liczba[2] . "','" . $liczba[3] . "','" . $liczba[4] . "','" . $liczba[5] . "')");
+                    $arr[] = "('" . $id . "','" . $data . "','" . $liczba[0] . "','" . $liczba[1] . "','" . $liczba[2] . "','" . $liczba[3] . "','" . $liczba[4] . "','" . $liczba[5] . "')";
+                }
+                
+                $chunks = array_chunk($arr, 500);
+                foreach ($chunks as $chunk) {
+                    mysqli_query($connection, "INSERT INTO `results`(`id`, `result-date`, `n1`, `n2`, `n3`, `n4`, `n5`, `n6`) VALUES " . implode(",", $chunk));
                 }
 
                 echo "<h1>Pomyślnie dodano 4000 wartości do tabelki. Przejdź <a href='repeats.php'>tutaj</a> aby zobaczyć jakie ile i jakie cyfry się powtarzają</h1>";
