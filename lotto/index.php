3b49d9183ee5b5eaff776def4094ef4034e55cc3
bkcf <ulatowski.bartlomiej@zs5.mail.pl>
2024-03-27 13:53:50 +0100

    komentarze (lotto)
    dodano komentarze do lotto + usunięto wyświetlanie daty

--- lotto/index.php
+++ lotto/index.php
@@ -9,32 +9,42 @@
         <br/>
 
         <?php
+            //niewyświetlanie błędów, łączenie z bazą danych
             error_reporting(0);
             $connection = mysqli_connect("localhost", "root", "", "lotto");
             $r = mysqli_query($connection, "SELECT * FROM `results`;");
             $row = mysqli_fetch_array($r);
+
+            //zmienna $plik ma wartość pobranego pliku
             $plik = file("C:\Users\Praktykant\Downloads\dl.txt");
 
+            //jeśli pierwsze id jest puste
             if(empty($row['id'])){
                 $arr = [];
+                //4000 razy zapisuje kawałki linijek pliku, do zmiennych, odzielonych tym, co jest pomiędzy ""
                 for($i = 0; $i <= 3999; $i++){
                     $linijka = explode(".", $plik[$i]);
                     $rok = explode(" ", $linijka[3]);
                     $liczba = explode(",", $rok[1]);
 
+                    //ustawia id i datę
                     $id = $linijka[0];
-                    echo $data = $rok[0] . "-" . $linijka[2] . "-" . trim($linijka[1]);
 
+                    //zapisuje wszystko do tablicy
                     $arr[] = "('" . $id . "','" . $data . "','" . $liczba[0] . "','" . $liczba[1] . "','" . $liczba[2] . "','" . $liczba[3] . "','" . $liczba[4] . "','" . $liczba[5] . "')";
                 }
                 
+                //zmienia array 4000 linijek na tablice po 500 na tablicę
                 $chunks = array_chunk($arr, 500);
+
+                //wpisuje wartości do bazy danych, masowo (po 500)
                 foreach ($chunks as $chunk) {
                     mysqli_query($connection, "INSERT INTO `results`(`id`, `result-date`, `n1`, `n2`, `n3`, `n4`, `n5`, `n6`) VALUES " . implode(",", $chunk));
                 }
 
                 echo "<h1>Pomyślnie dodano 4000 wartości do tabelki. Przejdź <a href='repeats.php'>tutaj</a> aby zobaczyć jakie ile i jakie cyfry się powtarzają</h1>";
             }
+            //jeśli pierwsze id NIE jest puste
             else{
                 echo "<h1>Dane już zostały wprowadzone. Kliknij <a href='repeats.php'>tutaj</a> aby zobaczyć jakie ile i jakie cyfry się powtarzają</h1>";
             }
--- lotto/repeats.php
+++ lotto/repeats.php
@@ -8,20 +8,28 @@
         <?php
             $connection = mysqli_connect('localhost', "root", "", "lotto");
 
+            //zakłąda że liczby są do 99, i tyle razy wykonuje się pętla
             for($i = 0; $i < 99; $i++){
+
+                //wyszukuje czy w w jakiejkolwiek kolumnie występuje poszczególna cyfra, a nastepnie ustala wartość zmiennej na ilość powtórzeń liczby równej indexowi
                 $repeats[$i] = 0;
                 $r = mysqli_query($connection, "SELECT * FROM `results` WHERE `n1` = $i OR `n2` = $i OR `n3` = $i OR `n4` = $i OR `n5` = $i OR `n6` = $i");
                 while($row = mysqli_fetch_array($r)){
                     $repeats[$i] ++;
                 }
 
+                //wyświetlanie liczby, i ile ma powtórzeń
                 echo "id: " . $i . ", powtórzenia: " . $repeats[$i] . "<br/>";
 
+                //ustawianie wartości w tabelce
                 $r = mysqli_query($connection, "SELECT * FROM `repeats`");
                 $row = mysqli_fetch_array($r);
+
+                //jeśli pierwsze id jest puste (sprawdza czy były już wpisywane).
                 if(!empty($row['id'])){
                     mysqli_query($connection, "INSERT INTO `repeats`(`number`, `repeats`) VALUES ('$i','" . $repeats[$i] . "')");
                 }
+                //jeśli id NIE jest puste, aktualizuje, jeśli coś by się zmieniło
                 else{
                     mysqli_query($connection, "UPDATE `repeats` SET `repeats`='" . $repeats[$i] . "' WHERE `number` = $i ");
                 }
